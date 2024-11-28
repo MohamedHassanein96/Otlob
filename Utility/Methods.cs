495c6651e1644959faf8c5f5d14547d04171f65d
MohamedHassanein96 <mohamedhassanein365@gmail.com>
2024-11-28 14:43:57 +0200

    Add Methods Class

---
+++ Utility/Methods.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class Methods
+    {
+        public static string? UploadImg(IFormFile PhotoUrl)
+        {
+            if (PhotoUrl.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(PhotoUrl.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    PhotoUrl.CopyTo(stream);
+                }
+                return fileName;
+            }
+            return null;
+        }
+    }
+}
